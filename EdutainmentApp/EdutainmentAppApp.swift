29648a4eaf3325c7798225d84ed45b824d4ad4e5
Artem Mandych <artemmandych@gmail.com>
2023-05-15 00:34:17 +0300

    Initial Commit

---
+++ EdutainmentApp/EdutainmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentAppApp.swift
+//  EdutainmentApp
+//
+//  Created by Artem Mandych on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
